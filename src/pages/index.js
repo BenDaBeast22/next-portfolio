4481758411418c7a149c020fd37fa875ef5c0832
BenDaBeast22 <bencritoph@gmail.com>
2026-02-09 21:54:24 -0500

    new cross cribbs deploy link

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ const projects = [
     p: "A responsive multiplayer cross cribbs app developped using TypeScript. I used React for the frontend framework, Node for the game server and Socket IO for multiplayer functionality. Users can play Cross Cribbs locally or multiplayer 1v1 or 2v2 in teams. In multiplayer users can host or join a game room to play together.",
     img: multiplayerCribbs,
     alt: "Bookmark saving site image",
-    link: "https://cross-cribbs.up.railway.app/",
+    link: "https://crosscribbs-multiplayer.onrender.com/",
   },
   {
     name: "Collection Listing Website",
